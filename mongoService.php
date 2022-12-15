09023643362dd63ad919e2d342b61a9c740eb5c6
dzmm38 <dennis.zimmer@mnd.thm.de>
2022-12-15 16:17:07 +0100

    rewrite mogoService for better usage
    first "correct" implementation

--- mongoService.php
+++ mongoService.php
@@ -4,27 +4,92 @@ require __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
+//TODO session management irgendwie machen damit es keine überschneidungen etc. gibt !!!!
+
 class MongoDBService {
-    private $client;
-    private $db;
+    private $client;    # Connection to The MongoDB instance
+    private $db;        # The actual Database of the instance
   
     public function __construct() {
-      $this->client = new MongoDB\Client($_ENV["dbConnection"]);
-      $this->db = $this->client->quizVerwaltung;
+        try {
+            # Connecting to the MongoDB using the dbConnection defined in the .env file
+            $this->client = new MongoDB\Client($_ENV["dbConnection"]);
+            # From the Client one Database is selected and only the DB is used for Operations
+            $this->db = $this->client->quizVerwaltung;
+
+        } catch (\Throwable $e) {
+            echo("Error writing in Database. <br>" . $e);
+        }
     }
-  
-    public function insert($collection, $data) {
-      $result = $this->db->$collection->insertOne($data);
+
+    /**
+     * Inserts a single php object into a mongodb collection.
+     *
+     * @param string     $collection     The collection name of the mongodb to which the data should be inserted
+     * @param object     $data           The data to be inserted as a php object
+     *
+     * @return //TODO which type     $result     The _id of the inserted Object whithin mongodb
+     */
+    public function insertSingle($collection, $data) {
+      try {
+        $result = $this->db->$collection->insertOne($data);
+      } catch (\Throwable $e) {
+        echo("Insertion Failed !!!");
+        return null;
+      }
+        
       return $result->getInsertedId();
     }
+
+    /**
+     * Inserts multiple php objects into a mongodb collection.
+     * 
+     * @param string        $collection     The collection name of the mongodb to which the data should be inserted
+     * @param array[object] $data           An array of php objects to be inserted into the db
+     * 
+     * @return array        $array           An array of vlaues which represents the _ids of the php objects in mongodb
+     */
+    public function insertMultiple($collection, $data) {
+        $array = [];
+        $result = $this->db->$collection->insertMany($data);
+        
+        # convert the curser object from mongodb into an array of ids of the objects
+        foreach ($result as $value) {
+            $array[] = $value;
+        }
+
+        return $array;
+    }
+
+    /**
+     * Function for finding the first entry in the db corresponding to the defined arguments
+     * 
+     * @param string        $collection     The collection name of the mongodb
+     * @param array[string] $filter         An array of assignments [id => 123, "name" => "xyz] defining filters
+     * @param array[string] $options        An array of assignments defining options
+     * 
+     * @return object       $result         Returns the first match as a php object
+     */
+    public function findSingle($collection, $filter, $options) {
+        $result = $this->db->$collection->findOne($filter, $options);
+    }
+
+    //TODO findMany
+    public function read($collection, $filter, $options) {
+        $results = $this->db->$collection->find($filter, $options);
+
+        $documents = [];
+        foreach ($results as $document) {
+            $documents[] = $document;
+        }
+        
+        return $documents;
+    }
+
+    //TODO delete by
+    
+    //TODO update 
+
+    //TODO vllt auch start und end session als extra function ?!?!? --> ansonsten in den einzelnen Funktionen machen 
   }
-
-
-  $mongo = new MongoDBService();
-  $id = $mongo->insert('questions', [
-    'name' => 'test Doe',
-    'email' => 'johndoe@example.com'
-  ]);
-
-
 ?>
