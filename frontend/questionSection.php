9ca83c6ba0f86183de13885b85c668ae6f50cf30
SilasKa98 <silas.kammerer@googlemail.com>
2023-01-28 21:00:31 +0100

    renamed file

--- frontend/questionSection.php
+++ frontend/questionSection.php
@@ -59,7 +59,7 @@ echo '</div>';
         transAndSave.addEventListener("click", function() {
             $.ajax({
                 type: "POST",
-                url: 'changeObjectService.php',
+                url: 'changeObjects.php',
                 data: {
                     selLanguage: selLanguage,
                     method: method,
@@ -85,7 +85,7 @@ echo '</div>';
         let method = "changeKarma";
         $.ajax({
                 type: "POST",
-                url: 'changeObjectService.php',
+                url: 'changeObjects.php',
                 data: {
                     job: job,
                     method: method,
