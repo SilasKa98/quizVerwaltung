5a4424446e2eb11b5090538a39401c42c2bdcca6
SilasKa98 <silas.kammerer@googlemail.com>
2023-02-02 07:48:37 +0100

    added toast notification to the index.php
    toast can be called via js/jquery with: $(".toast").toast('show');

--- index.php
+++ index.php
@@ -81,5 +81,25 @@ include "frontend/questionSection.php";
 ?>
 
   </div>
+<!--end if container-fluid-->
+
+
+
+
+  <!--notification Toast to show all sorts of notifications, can be called with this: $(".toast").toast('show');-->
+  <div class="position-fixed bottom-0 end-0 p-3" style="z-index: 11">
+    <div id="liveToast" class="toast hide" role="alert" aria-live="assertive" aria-atomic="true">
+      <div class="toast-header">
+        <img src="media/logo.jpg" width="20px" class="rounded me-2" alt="our logo">
+        <strong class="me-auto">Quiz-Verwaltung</strong>
+        <small><?php echo $toastTimeDisplay; ?></small>
+        <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+      </div>
+      <div class="toast-body" id="toastMsgBody">
+       
+      </div>
+    </div>
+  </div>
+
 </body>
 </html>
