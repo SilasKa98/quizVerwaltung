af480da06ebbe6a4dc83f9eb8728c12a889fb032
dzmm38 <dennis.zimmer@mnd.thm.de>
2023-02-06 15:30:55 +0100

    included catalogCart.php for later use

--- frontend/navbar.php
+++ frontend/navbar.php
@@ -1,9 +1,12 @@
+<?php include_once "catalogCart.php";?>
+
 <nav class="navbar navbar-expand-lg bg-body-tertiary sticky-top">
     <div class="container-fluid">
         <a class="navbar-brand" href="/quizVerwaltung/index.php">
             <img src="/quizVerwaltung/media/logoQuizManagerTransparent2.png" alt="Logo" width="80"  class="d-inline-block align-text-top">
             <p id="headerName">Quiz Manager</p>
         </a>
+        
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav">
                 <a class="nav-link active" aria-current="page" href="/quizVerwaltung/frontend/frontend_insertQuestion.php"><?php echo $navText_insertQuestion ?></a>
