028fe6377bb2de44372a090fbff50f0c3f3348f4
dzmm38 <dennis.zimmer@mnd.thm.de>
2023-02-06 15:25:51 +0100

    added a cart like offcanvas
    only basic structure without function

---
+++ frontend/catalogCart.php
@@ -0,0 +1,27 @@
+<div class="offcanvas offcanvas-end" data-bs-scroll="true" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+  <div class="offcanvas-header">
+    <!--<h5 class="offcanvas-title" id="offcanvasRightLabel">Fragenkatalog</h5>-->
+    <button type="button" class="btn btn-light" style="--bs-btn-font-size: 1.25rem;">Fragenkatalog</button>
+    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+    <p>TODO Hier kommen dann die einzelnen Fragen rein die der user in seinem Warenkorb hat.</p> <!-- //TODO das was da steht -->
+  </div>
+
+  <div class="container-fluid" align="center" style="margin-bottom: 10px;" id=catalogButtons>
+    <button type="button" class="btn btn-primary" style="width: 40%;">Katalog erstellen</button>
+    
+    <div class="btn-group">
+      <button type="button" class="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false" style="width: 40%;">
+      Exportieren
+      </button>
+        <ul class="dropdown-menu">
+        <li><a class="dropdown-item" href="TODO">Moodle</a></li>
+        <li><a class="dropdown-item" href="TODO">TODO</a></li> <!-- //TODO noch weitere Formate dann einfügen -->
+        <li><hr class="dropdown-divider"></li>
+        <li><a class="dropdown-item" href="TODO">Standard</a></li>
+      </ul>
+    </div>
+
+  </div>
+</div>
