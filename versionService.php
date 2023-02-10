c0143258d707982fe7515318871c539209f720d5
SilasKa98 <silas.kammerer@googlemail.com>
2023-02-10 08:16:07 +0100

    fixed a bug where version wouldn't increase correctly

--- versionService.php
+++ versionService.php
@@ -13,7 +13,12 @@ class VersionService {
         $parts = explode('.', $oldVersion);
         $major = (int) $parts[0];
         $minor = isset($parts[1]) ? (int) $parts[1] : 0;
-        $minor++;
+        if($minor != 9){
+           $minor++; 
+        }else{
+            $minor = 0;
+            $major++;
+        } 
         $this->version = "$major.$minor";
         return $this->version;
     }
