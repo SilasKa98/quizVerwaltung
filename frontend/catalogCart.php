33d65f50d8472d060770d2c30f7d2bb1a331ecad
dzmm38 <dennis.zimmer@mnd.thm.de>
2023-02-21 14:10:17 +0100

    code cleanup
    export button no longer a dropdown
    added modal function to the export button

--- frontend/catalogCart.php
+++ frontend/catalogCart.php
@@ -1,7 +1,6 @@
 <div class="offcanvas offcanvas-end" data-bs-scroll="true" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
   <div class="offcanvas-header">
     <h5 class="offcanvas-title" id="offcanvasRightLabel">Fragenkorb</h5>
-    <!--<button type="button" class="btn btn-light" style="--bs-btn-font-size: 1.25rem;">Fragenkorb</button>-->
     <button type="button" class="btn btn-light clearCart" name="clearCart" style="margin-right: auto;" onclick="emptyQuestionCart()">
       <img src="/quizVerwaltung/media/trash-can.svg" width="20px"/>
     </button>
@@ -19,24 +18,18 @@
   </div>
 
   <div class="container-fluid" align="center" style="margin-bottom: 10px;" id=catalogButtons>
-    <!--<button type="button" name="exportButton" class="btn btn-primary" style="width: 40%;" onclick="createCatalog()">Katalog erstellen</button>-->
     <button type="button" name="exportButton" class="btn btn-primary" style="width: 40%;" data-bs-toggle="modal" data-bs-target="#catalogOptions" onclick="createCatalog(this)">
       Katalog erstellen
     </button>
     
-    <div class="btn-group">
-      <button type="button" class="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false" style="width: 40%;">
-      Exportieren
-      </button>
-        <ul class="dropdown-menu">
-        <li><a class="dropdown-item" href="TODO">Moodle</a></li>
-        <li><a class="dropdown-item" href="TODO">TODO</a></li> <!-- //TODO noch weitere Formate dann einfügen -->
-        <li><hr class="dropdown-divider"></li>
-        <li><a class="dropdown-item" href="TODO">Standard</a></li>
-      </ul>
-    </div>
+    <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exportDownload" aria-expanded="false" style="width: 40%;">
+    Exportieren
+    </button>
 
   </div>
 </div>
 
-<?php include_once("modal_catalogOptions.php"); ?>
+<?php 
+  include_once("modal_catalogOptions.php"); 
+  include_once("modal_downloadExport.php");  
+?>
