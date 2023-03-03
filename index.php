cf59928a4bed47cd084fae45fbd4de0ab5467811
SilasKa98 <silas.kammerer@googlemail.com>
2023-03-03 12:17:28 +0100

    refactoring

--- index.php
+++ index.php
@@ -32,6 +32,13 @@
     <script src="https://code.jquery.com/jquery-3.6.2.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
+    <!-- fly to card animation scripts -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+    <script src="/quizVerwaltung/scripts/flyToCartAnimation.js"></script>
+    <script src="/quizVerwaltung/scripts/questionScripts.js"></script>
+    <script src="/quizVerwaltung/scripts/searchSystemScript.js"></script>
+
 </head>
 <body onload="getLatestQuestionsOfFollowedUsers();getPersonRecommendations()">
 <?php include_once "frontend/navbar.php";?>
@@ -95,11 +102,8 @@
 <?php include_once "frontend/notificationToast.php";?>
 
 
-<!-- fly to card animation scripts -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
-<script src="/quizVerwaltung/scripts/flyToCartAnimation.js"></script>
-<script src="/quizVerwaltung/scripts/questionScripts.js"></script>
-<script src="/quizVerwaltung/scripts/searchSystemScript.js"></script>
+
+
 </body>
 
 </html>
